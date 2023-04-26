b9960501accc4693c63102de5e3aab4df332ef44
quanchan <quan.trananh12@gmail.com>
2023-04-26 20:12:58 +1000

    Switch back to using trpc instead of open-api

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,12 +1,10 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { createOpenApiNextHandler } from 'trpc-openapi';
-
 import { env } from "@/env.mjs";
 import { createTRPCContext } from "@/server/api/trpc";
 import { appRouter } from "@/server/api/root";
 
 // export API handler
-export default createOpenApiNextHandler({
+export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
   onError:
