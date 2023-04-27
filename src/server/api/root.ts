43bdc218454f85a36f40a0c7fab9fed9a5d86a1e
huukhang1512 <huukhang1512@gmail.com>
2023-04-28 02:23:09 +1000

    trial with changing user profile info

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,15 @@
 import { createTRPCRouter } from "@/server/api/trpc";
+<<<<<<< HEAD
 import { exampleRouter } from "@/server/api/routers/example";
 import { userRouter } from "./routers/user";
 import { serviceRouter } from "./routers/service";
 import { requestRouter } from "./routers/request";
 import { proposalRouter } from "./routers/proposal";
 import { jobRouter } from "./routers/job";
+=======
+import { tradieRouter } from "@/server/api/routers/tradie";
+import { userRouter } from "./routers/user";
+>>>>>>> 28fbfea (trial with changing user profile info)
 
 /**
  * This is the primary router for your server.
@@ -12,12 +17,17 @@ import { jobRouter } from "./routers/job";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+<<<<<<< HEAD
   example: exampleRouter,
   users: userRouter,
   services: serviceRouter,
   requests: requestRouter,
   proposals: proposalRouter,
   jobs: jobRouter,
+=======
+  tradie: tradieRouter,
+  user: userRouter,
+>>>>>>> 28fbfea (trial with changing user profile info)
 });
 
 // export type definition of API
