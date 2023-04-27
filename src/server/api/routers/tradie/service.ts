f44b6b2823019cf0d28a45caef3f9becd9274cfc
huukhang1512 <huukhang1512@gmail.com>
2023-04-27 21:27:34 +1000

    add Service migration

--- src/server/api/routers/tradie/service.ts
+++ src/server/api/routers/tradie/service.ts
@@ -1,38 +1,35 @@
 import { z } from "zod";
 import { prisma } from "@/server/db";
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "@/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 
-const Services = z.string().array();
+const ChooseServicesRequest = z.string().array();
 
 export const serviceRouter = createTRPCRouter({
-  chooseServices: protectedProcedure
-    .input(Services)
-    .mutation( async (req) => {
-      const { input: services, ctx } = req;
-      const { id } = ctx.session.user;
-      const validServices = await prisma.service.findMany({
-        where: {
-          name: {
-            in: services,
-          }
+  chooseServices: protectedProcedure.input(ChooseServicesRequest).mutation(async (req) => {
+    const { input: services, ctx } = req;
+    const { id } = ctx.session.user;
+
+    const validServices = await prisma.service.findMany({
+      where: {
+        name: {
+          in: services,
         },
-        select: {
-          name: true
-        }
-      })
-      const updatedUser = await prisma.user.update({
-        where: {
-          id: id
+      },
+      select: {
+        name: true,
+      },
+    });
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        providedServices: {
+          set: validServices,
         },
-        data: {
-          providedServices: {
-            set: validServices
-          }
-        }
-      })
-      return updatedUser;
-    }),
+      },
+    });
+    return updatedUser;
+  }),
 });
