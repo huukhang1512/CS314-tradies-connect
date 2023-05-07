3dff19bfb132bb2a4b9f48ae8345407013e9bed0
Huu Khang Nguyen <huukhang1512@gmail.com>
2023-05-07 20:46:59 +1000

    create generic getServerSidePropsWithAuth (#12)

--- src/pages/app/client/index.tsx
+++ src/pages/app/client/index.tsx
@@ -1,7 +1,5 @@
 import SidebarWithHeader from "@/components/SidebarWithHeader";
 import { Portal } from "@/components/SidebarWithHeader";
-import { getServerSideProps as redirect } from "@/pages/app";
-export const getServerSideProps = redirect;
 
 const Client = () => {
   return (
@@ -10,4 +8,6 @@ const Client = () => {
     </>
   );
 };
+
 export default Client;
+export { getServerSidePropsWithAuth as getServerSideProps } from "@/components/getServerSidePropsWithAuth";
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,9 +1,5 @@
 import AppBar from "@/components/AppBar";
-import { authOptions } from "@/server/auth";
 import { Heading, Stack, Text, VStack } from "@chakra-ui/react";
-import { type GetServerSidePropsContext } from "next";
-import { getServerSession } from "next-auth";
-import { Role } from "@prisma/client";
 
 import customerSiteSVG from "@/assets/customerSite.svg";
 import tradieSiteSVG from "@/assets/tradieSite.svg";
@@ -66,20 +62,5 @@ const App = () => {
   );
 };
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
-  if (session?.user.role === Role.ADMIN) {
-    return { redirect: { destination: "/admin" } };
-  }
-  // If the user is not logged in, redirect.
-  if (!session) {
-    return { redirect: { destination: "/" } };
-  }
-  return {
-    props: { session },
-  };
-};
-
 export default App;
+export { getServerSidePropsWithAuth as getServerSideProps } from "@/components/getServerSidePropsWithAuth";
