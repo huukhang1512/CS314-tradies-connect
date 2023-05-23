0f8e52226871396f0edce113c9f4c223687e7700
Huu Khang Nguyen <huukhang1512@gmail.com>
2023-05-24 01:34:02 +1000

    add faker for fake data generation (#23)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,28 @@
 import { CLIENT_MEMBERSHIP, TRADIE_MEMBERSHIP } from "@/constant";
+import { generateFakeRequestsFromUsersAndServices } from "@/fakes/fakeRequest";
 import { SERVICES } from "@/fakes/fakeServices";
+import { createFakeUsers } from "@/fakes/fakeUser";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 async function main() {
+  const fakeUsers = await createFakeUsers(30);
+  const users = await prisma.user.createMany({ data: fakeUsers });
   const services = await prisma.service.createMany({ data: SERVICES });
   const memberships = await prisma.membership.createMany({
     data: [TRADIE_MEMBERSHIP, CLIENT_MEMBERSHIP],
   });
+  const fakeRequests = generateFakeRequestsFromUsersAndServices(
+    fakeUsers,
+    SERVICES
+  );
+  const requests = await prisma.request.createMany({
+    data: fakeRequests,
+  });
+  console.log("Users seeded", users);
   console.log("Services seeded", services);
   console.log("Memberships seeded", memberships);
+  console.log("Requests seeded", requests);
 }
 main()
   .then(async () => {
