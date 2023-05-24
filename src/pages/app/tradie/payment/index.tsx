793f07991584eb44266223ce030e9953be9542c8
huukhang1512 <huukhang1512@gmail.com>
2023-05-25 00:36:11 +1000

    add table to display the payment table in tradie and client

---
+++ src/pages/app/tradie/payment/index.tsx
@@ -0,0 +1,70 @@
+import SidebarWithHeader from "@/components/SidebarWithHeader";
+import { Portal } from "@/components/SidebarWithHeader";
+import CustomTable from "@/components/Table";
+import { api } from "@/utils/api";
+import { Badge, Card } from "@chakra-ui/react";
+import { type Payment, PaymentStatus } from "@prisma/client";
+import { useCallback, useMemo, useState } from "react";
+import { type Column } from "react-table";
+
+const TradiePayment = () => {
+  const { mutateAsync } = api.payments.getClientRequestPayment.useMutation();
+  const [forceRefetch] = useState(true);
+
+  const getData = useCallback(
+    async (page = 1, perPage = 10) => {
+      return await mutateAsync({ page, perPage });
+    },
+    [mutateAsync]
+  );
+  const columns = useMemo<Column<Payment>[]>(
+    () => [
+      {
+        Header: "PAYMENT TYPE",
+        accessor: "paymentType",
+      },
+      {
+        Header: "AMOUNT",
+        accessor: "amount",
+        Cell: ({ value }) => {
+          return <>${value}</>;
+        },
+      },
+      {
+        Header: "STATUS",
+        accessor: "paymentStatus",
+        Cell: ({ value }) => {
+          switch (value) {
+            case PaymentStatus.COMPLETED:
+              return <Badge colorScheme={"green"}>{value}</Badge>;
+            case PaymentStatus.PENDING:
+              return <Badge colorScheme={"gray"}>{value}</Badge>;
+            default:
+              return <Badge colorScheme={"red"}>{value}</Badge>;
+          }
+        },
+      },
+      {
+        Header: "DATE",
+        accessor: "createdAt",
+        Cell: ({ value }) => <>{value.toLocaleDateString()}</>,
+      },
+    ],
+    []
+  );
+
+  return (
+    <SidebarWithHeader portal={Portal.TRADIE}>
+      <Card p={5}>
+        <CustomTable
+          refetchState={forceRefetch}
+          getData={getData}
+          columns={columns}
+        />
+      </Card>
+    </SidebarWithHeader>
+  );
+};
+
+export default TradiePayment;
+export { getServerSidePropsWithAuth as getServerSideProps } from "@/components/getServerSidePropsWithAuth";
--- src/server/api/routers/payment.ts
+++ src/server/api/routers/payment.ts
@@ -4,6 +4,7 @@ import {
   protectedProcedure,
 } from "@/server/api/trpc";
 import { PaginatedInput } from "@/types/paginatedInput";
+import { PaymentType } from "@prisma/client";
 
 export const paymentRouter = createTRPCRouter({
   getUserPayments: protectedProcedure.query(async (req) => {
@@ -53,4 +54,58 @@ export const paymentRouter = createTRPCRouter({
       data: payments,
     };
   }),
+
+  getClientRequestPayment: protectedProcedure
+    .input(PaginatedInput)
+    .mutation(async (req) => {
+      const { prisma } = req.ctx;
+      const payments = await prisma.payment.findMany({
+        include: {
+          User: true,
+        },
+        where: {
+          paymentType: PaymentType.REQUEST,
+        },
+        skip: (req.input.page - 1) * req.input.perPage,
+        take: req.input.perPage,
+      });
+      const total = await prisma.payment.count({
+        where: {
+          paymentType: PaymentType.REQUEST,
+        },
+      });
+      return {
+        total,
+        page: req.input.page,
+        perPage: req.input.perPage,
+        data: payments,
+      };
+    }),
+
+  getTradieJobPayoutPayment: protectedProcedure
+    .input(PaginatedInput)
+    .mutation(async (req) => {
+      const { prisma } = req.ctx;
+      const payments = await prisma.payment.findMany({
+        include: {
+          User: true,
+        },
+        where: {
+          paymentType: PaymentType.JOB_PAYOUT,
+        },
+        skip: (req.input.page - 1) * req.input.perPage,
+        take: req.input.perPage,
+      });
+      const total = await prisma.payment.count({
+        where: {
+          paymentType: PaymentType.JOB_PAYOUT,
+        },
+      });
+      return {
+        total,
+        page: req.input.page,
+        perPage: req.input.perPage,
+        data: payments,
+      };
+    }),
 });
